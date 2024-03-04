7bfa39a366054e5cae93f60cac1efb87c8149524
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-04 23:21:06 +0700

    add boostrap, jquery, datatables library

---
+++ app/Views/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? 'NoTitle' ?></title>
+    <meta name="description" content="Aplikasi Sederhana - Phiraka Test">
+    <!-- Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <!-- DataTables Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
+</head>
+
+<body>
