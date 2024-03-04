171d35a6b6bc916dba739fdcc3ce2ec2cbe76358
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-04 19:44:41 +0700

    add tbl_user from Migration

---
+++ app/Database/Migrations/2024-03-04-123726_CreateUsersTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '54',
+            ],
+            'CreateTime' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tbl_user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbl_user');
+    }
+}
